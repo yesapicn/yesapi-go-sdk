2f8ddae32184834f8e0132b0910adb3d2de7ef93
hexiaohong <610087273@qq.com>
2019-09-21 09:41:48 +0800

    单元测试

---
+++ YesApiCilent/YesApiCilent_test.go
@@ -0,0 +1,22 @@
+package YesApiCilent
+
+import "testing"
+
+/**
+ * YesApi客户端单元测试
+ * @package   YesApiCilent
+ * @author    sHuXnHs <610087273@qq.com>
+ */
+
+func TestDoRequest(t *testing.T) {
+	param := map[string]string{"s": "App.Hello.World" , "name" : "HXH"}
+	rs, err := DoRequest(param)
+	if err != nil {
+		t.Log("请求失败")
+		t.FailNow()
+	} else {
+		t.Log("code：", rs.Code)
+		t.Log("data：", rs.Data)
+		t.Log("msg：", rs.Msg)
+	}
+}
